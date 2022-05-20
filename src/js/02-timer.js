44a412066ba061f22015d5bf3a91faa83cb34f22
AndreyMachula <amcz2020@gmail.com>
2022-05-20 02:50:11 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,26 +6,32 @@ require("flatpickr/dist/themes/dark.css");
 const refs = {
   PickerDateTime: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector("[data-start]"),
-  deltaTime: document.querySelector('.timer'),
+  clockface: document.querySelector('.timer'),
   deltaDays: document.querySelector('[data-days]'),
   deltaHours: document.querySelector('[data-hours]'),
   deltaMinutes: document.querySelector('[data-minutes]'),
   deltaSeconds: document.querySelector('[data-seconds]'),
 };
 
-console.log(refs);
+// console.log(refs);
 
-refs.startBtn.disabled = true;
 let selectedTime = null;
 let selectedDate = null;
 let intervalId = null;
 const dateNow = new Date();
+refs.startBtn.disabled = true;
 
+// Object parameters
 const options = {
+  // Includes time picker
   enableTime: true,
+  // Displays the clock in 24-hour format without selecting AM/PM if enabled
   time_24hr: true,
+  // Sets the selected start date
   defaultDate: new Date(),
+  // Adjusts the minute input increment (including scrolling)
   minuteIncrement: 1,
+  // Called when the UI element that flatpickr creates closes
   onClose(selectedDates) {
     if (selectedDates[0] < Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
@@ -41,71 +47,75 @@ flatpickr(refs.PickerDateTime, options)
 // console.log(flatpickr);
 
 class Timer {
-  constructor() {
-    this.timerID = null;
+  constructor({onTick}) {
+    this.intervalID = null;
     this.isActive = false;
-    // this.deltaTime = 0;
-    refs.startBtn.disabled = true;
+    this.onTick = onTick;
+    refs.startBtn.disabled = true
   }
 
+  // Start countdown timer
   startTimer() {
     if (this.isActive) {
       return;
     }
 
     this.isActive = true;
-    this.timerID = setInterval(() => {
+
+    this.intervalID = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = selectedTime - currentTime;
-      const componentsTimer = convertMs(deltaTime);
-      this.updateComponentsTimer(componentsTimer);
+      const timerComponents = this.convertMs(deltaTime);
+      // Calls a function that passes the time
+      this.onTick(timerComponents)
       if (deltaTime <= 0) {
         this.stopTimer();
       }
     }, 1000);
   }
-
-  updateComponentsTimer({ days, hours, minutes, seconds }) {
-    refs.deltaDays.textContent = days;
-    refs.deltaHours.textContent = hours;
-    refs.deltaMinutes.textContent = minutes;
-    refs.deltaSeconds.textContent = seconds;
-  }
-
+  
+  // End countdown timer
   stopTimer() {
-    clearInterval(this.timerID);
+    clearInterval(this.intervalID);
+    this.isActive = false;
   }
-}
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    
+    // Remaining days
+    const days = this.pad(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.pad(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+    return { days, hours, minutes, seconds };
+  }
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-const addLeadingZero = value => {
-  return String(value).padStart(2, '0');
+  pad = value => {
+    return String(value).padStart(2, '0');
+  }
 };
 
+const timer = new Timer({
+  onTick: updateClockface
+});
 
-const timer = new Timer();
+//Draws the interface
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.deltaDays.textContent = days;
+  refs.deltaHours.textContent = hours;
+  refs.deltaMinutes.textContent = minutes;
+  refs.deltaSeconds.textContent = seconds;
+}
+
+
 
 refs.startBtn.addEventListener('click', () => {
   timer.startTimer();
